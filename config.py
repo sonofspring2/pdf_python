3f9fb38887cb2cef930b2e65b165181f4917c4a4
sonofpring2 <mdai3@ncsu.edu>
2017-03-20 21:04:13 -0400

    create pdf extract function

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from easydict import EasyDict as edict
 
 default = edict()
-default.input_file = './input/test.pdf'
+default.input_file = './input/photocopy.pdf'
 default.output_folder = './output'
 default.verbose = True
 default.overwrite = True
--- pdf_manipulate.py
+++ pdf_manipulate.py
@@ -28,8 +28,37 @@ def pdfSplit(input_pdf, output_folder):
     return
 
 
+def pdfExtract(input_pdf,output_folder,page_range=(0,1)):
+    assert os.path.exists(input_pdf), " pdf file {} does not exist!".format(input_pdf)
+    infile = PdfFileReader(open(input_pdf, 'rb'))
+
+    if default.overwrite and os.path.exists(output_folder):
+        shutil.rmtree(output_folder)
+
+    if not os.path.exists(output_folder):
+        os.mkdir(output_folder)
+
+    num_pages = infile.getNumPages()
+    assert page_range[1] < num_pages, 'extract page({}) should be less than the total pages({})!'.format(page_range[1],num_pages)
+
+    outfile = PdfFileWriter()
+    for i_page in range(page_range[0],page_range[1]):
+        outfile.addPage(infile.getPage(i_page))
+
+    output_pdf = os.path.join(output_folder,'extract_range_{}_{}'.format(page_range[0],page_range[1]) +'.pdf' )
+    if default.verbose:
+        print 'output file: ', output_pdf
+
+    with open(output_pdf,'wb') as f:
+        outfile.write(f)
+
+
+    return
+
+
 def main():
-    pdfSplit(default.input_file,default.output_folder)
+    # pdfSplit(default.input_file,default.output_folder)
+    pdfExtract(default.input_file, default.output_folder, page_range=(64,88))
     return
 
 
